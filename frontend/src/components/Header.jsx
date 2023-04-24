5a08d1ffe78430789ca5d8bacd71150ff74120f4
Philip Kristian Møller Flyvholm <phif@itu.dk>
2023-04-24 15:53:23 +0200

    Fix: removed unused link in nav

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,13 +2,12 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 export default function Header() {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Navbar bg="dark" variant="dark" expand="lg" className='px-2 px-sm-5'>
+            <Navbar.Brand href="/">TwitterClone</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Link href="/mytweets">My Tweets</Nav.Link>
-                    <Nav.Link href="/publictweets">Public Tweets</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
